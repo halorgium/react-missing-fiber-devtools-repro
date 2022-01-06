de1a62ffd15948f34fd5c09ca3632a4ff66bc1ad
Tim Carey-Smith <tim@nzpdx.us>
2022-01-06 12:39:27 -0800

    Extract selector reducer and component

--- src/SelectedGame.tsx
+++ src/SelectedGame.tsx
@@ -2,32 +2,10 @@ import { useParams, useNavigate, generatePath } from "react-router-dom"
 import Game from './Game'
 import allPieces from './data/pieces'
 import allBoards from './data/boards'
-import { BoardData, BoardName, GameName, PieceData, PieceName, Positions, WindowEvent } from "./types"
-import { Reducer, useCallback, useEffect, useReducer } from "react"
-import { GridStore } from "./GridMap"
-
-interface SelectorProps {
-  current: string | null
-  options: string[]
-  actionType: SelectionActionType
-  dispatch: React.Dispatch<SelectionAction>
-}
-
-function Selector({ current, options, actionType, dispatch }: SelectorProps): JSX.Element {
-  const onSelect = useCallback<(event: WindowEvent) => void>((event: WindowEvent) => {
-    dispatch({
-      type: actionType,
-      choice: event.target.value === "" ? null : event.target.value,
-    })
-  }, [actionType, dispatch])
-
-  return (
-    <select value={current === null ? "" : current} onChange={onSelect}>
-      <option value=''>-- choose --</option>
-      {options.map(k => <option key={k} value={k}>{k}</option>)}
-    </select>
-  )
-}
+import { BoardData, BoardName, GameName, PieceData, PieceName } from "./types"
+import { useEffect } from "react"
+import useSelectionReducer, { SelectionActionType, SelectionDispatch, SelectionState } from "./useSelectionReducer"
+import Selector from "./Selector"
 
 function useBoardAndGame(): [BoardName | null, GameName | null] {
   let { board, game } = useParams()
@@ -45,144 +23,12 @@ function useBoardAndGame(): [BoardName | null, GameName | null] {
   return [board, game]
 }
 
-type SelectionReducer = Reducer<SelectionState, SelectionAction>
-
-enum SelectionActionType {
-  selectBoard,
-  selectLayout,
-}
-
-interface SelectionAction {
-  type: SelectionActionType
-  choice: string | null
-}
-
-interface SelectionInit extends SelectionBase {
-  boardParam: BoardName | null
-  layoutParam: GameName | null
-}
-
-interface SelectionBase {
-  pieces: Map<PieceName, PieceData>
-  boards: Map<BoardName, BoardData>
-}
-
-interface SelectionState extends SelectionBase {
-  currentBoard: BoardName | null
-  boardOptions: BoardName[]
-  currentLayout: GameName | null
-  layoutOptions: GameName[] | null
-  game: GameState | null
-}
-
-interface GameState {
-  layout: GameName
-  tiles: GridStore
-  pieces: Map<PieceName, PieceData>
-  positions: Positions
-}
-
-function reducer(state: SelectionState, action: SelectionAction): SelectionState {
-  switch (action.type) {
-    case SelectionActionType.selectBoard:
-      if (state.currentBoard === action.choice) {
-        return state
-      }
-
-      if (action.choice === null) {
-        return {
-          ...state,
-          currentBoard: null,
-          currentLayout: null,
-          layoutOptions: null,
-          game: null,
-        }
-      }
-
-      const newBoardData = state.boards.get(action.choice)
-
-      if (newBoardData === undefined) {
-        throw new Error("unknown board")
-      }
-
-      return {
-        ...state,
-        currentBoard: action.choice,
-        currentLayout: null,
-        layoutOptions: Array.from(newBoardData.positions.keys()),
-        game: null,
-      }
-    case SelectionActionType.selectLayout:
-      if (state.currentLayout === action.choice) {
-        return state
-      }
-
-      if (action.choice === null) {
-        return {
-          ...state,
-          currentLayout: null,
-          game: null,
-        }
-      }
-
-      if (state.currentBoard === null) {
-        throw new Error("board not selected")
-      }
-
-      const currentBoardData = state.boards.get(state.currentBoard)
-
-      if (currentBoardData === undefined) {
-        throw new Error("board is missing")
-      }
-
-      // TODO: rename to layout
-      const positions = currentBoardData.positions.get(action.choice)
-
-      if (positions === undefined) {
-        throw new Error("unknown layout")
-      }
-
-      return {
-        ...state,
-        currentLayout: action.choice,
-        game: {
-          layout: action.choice,
-          // TODO: show a board without pieces
-          tiles: currentBoardData.tiles,
-          pieces: state.pieces,
-          positions: positions,
-        },
-      }
-    default:
-      throw new Error('unknown action');
-  }
-}
-
-function init({ pieces, boards, boardParam, layoutParam }: SelectionInit): SelectionState {
-  const initialState = {
-    pieces,
-    boards,
-    currentBoard: null,
-    boardOptions: Array.from(boards.keys()),
-    currentLayout: null,
-    layoutOptions: null,
-    game: null,
-  }
-
-  const selectBoardState = reducer(initialState, { type: SelectionActionType.selectBoard, choice: boardParam })
-  return reducer(selectBoardState, { type: SelectionActionType.selectLayout, choice: layoutParam })
-}
-
-function useSelection(pieces: Map<PieceName, PieceData>, boards: Map<BoardName, BoardData>): [SelectionState, React.Dispatch<SelectionAction>] {
+function useSelection(pieces: Map<PieceName, PieceData>, boards: Map<BoardName, BoardData>): [SelectionState, SelectionDispatch] {
   const navigate = useNavigate()
 
   const [boardParam, layoutParam] = useBoardAndGame()
 
-  const [state, dispatch] = useReducer<SelectionReducer, SelectionInit>(
-    reducer,
-    { pieces, boards, boardParam, layoutParam },
-    init
-  )
+  const [state, dispatch] = useSelectionReducer(pieces, boards, boardParam, layoutParam)
 
   useEffect(() => {
     if (state.currentBoard === null) {
