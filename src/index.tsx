162f032cc3972085405ff32cea45e7c71d3315af
Tim Carey-Smith <tim@nzpdx.us>
2022-01-06 16:51:31 -0800

    single file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import { Stage, Layer, Text } from 'react-konva'
+
+function Debug(): JSX.Element {
+  return (
+    <>
+      <Text fontFamily='Courier' text='foo' />
+    </>
+  )
+}
+
+function Game(): JSX.Element {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Debug />
+      </Layer>
+    </Stage>
+  )
+}
+
+function App(): JSX.Element {
+  return (
+    <Game />
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
